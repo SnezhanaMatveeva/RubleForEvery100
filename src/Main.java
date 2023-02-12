e0c2558af638df62a7348752dfc24c3e15287225
SnezhanaMatveeva <12345fam@mail.ru>
2023-02-12 17:59:37 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        double startingScore=450;
+        double replenishmentAmount=999;
+        double totalAmount=startingScore+replenishmentAmount;
+        double bonus= totalAmount/100 ;
+        double totalAmountWithBonus;
+
+        totalAmount = startingScore + replenishmentAmount;
+
+        if (replenishmentAmount > 1000) {
+            totalAmountWithBonus = totalAmount + bonus;
+
+        } else {
+            totalAmountWithBonus = totalAmount;
+        }
+
+        System.out.println("Итоговый счет:" + " " + totalAmountWithBonus);
+        System.out.println("Бонусы:" + " " + bonus);
+    }
+}
